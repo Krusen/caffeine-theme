9abf915a41d390d8189bf241160d475bfda7f1e4
Søren Kruse <krusenen@gmail.com>
2020-02-18 21:22:23 +0100

    Change/fix fonts

--- assets/js/src/fonts.js
+++ assets/js/src/fonts.js
@@ -1,24 +1,26 @@
 "use strict";
 
 (function (w) {
-    var font1 = new w.FontFaceObserver("Raleway", {
+    
+    var link = document.createElement("link");
+    link.href = "https://fonts.googleapis.com/css?family=Open+Sans:400,600|Roboto+Slab&display=swap&subset=latin-ext";
+    link.rel = 'stylesheet';
+    document.head.appendChild(link);
+
+    var font1 = new w.FontFaceObserver("Open Sans", {
         weight: 400
     });
 
-    var font2 = new w.FontFaceObserver("Raleway", {
-        weight: 700
+    var font2 = new w.FontFaceObserver("Open Sans", {
+        weight: 600
     });
 
     var font3 = new w.FontFaceObserver("Roboto Slab", {
-        weight: 300
-    });
-
-    var font4 = new w.FontFaceObserver("Roboto Slab", {
         weight: 400
     });
 
     w.Promise
-        .all([font1.load(), font2.load(), font3.load(), font4.load()])
+        .all([font1.load(), font2.load(), font3.load()])
         .then(function () {
             if (w.document.documentElement.className.indexOf("fonts-loaded") == -1) {
                 w.document.documentElement.className += " fonts-loaded";
